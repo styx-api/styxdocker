536e9df528ce8a61647626d1820836f9e4c61348
Florian Rupprecht <floruppr@gmail.com>
2024-06-16 19:18:35 -0400

    Restructure

--- src/styxdocker/__init__.py
+++ src/styxdocker/__init__.py
@@ -1,14 +1,17 @@
 """.. include:: ../../README.md"""  # noqa: D415
 
+import logging
+import os
+import pathlib
 import pathlib as pl
 import re
+import shlex
 from collections import deque
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
-from subprocess import PIPE, CalledProcessError, Popen
-from typing import Callable
+from subprocess import PIPE, Popen
 
-from styxdefs import Execution, Metadata, Runner
+from styxdefs import Execution, InputPathType, Metadata, OutputPathType, Runner
 
 
 def _docker_mount(host_path: str, container_path: str, readonly: bool) -> str:
@@ -21,19 +24,46 @@ def _docker_mount(host_path: str, container_path: str, readonly: bool) -> str:
     return f"type=bind,source={host_path},target={container_path}{readonly_str}"
 
 
-class DockerExecution(Execution):
+class StyxDockerError(Exception):
+    """Styx Docker error."""
+
+    def __init__(
+        self,
+        return_code: int | None = None,
+        docker_args: list[str] | None = None,
+        command_args: list[str] | None = None,
+    ) -> None:
+        """Create StyxDockerError."""
+        message = "Command failed."
+
+        if return_code is not None:
+            message += f"\n- Return code: {return_code}"
+
+        if docker_args is not None:
+            message += f"\n- Docker args: {shlex.join(docker_args)}"
+
+        if command_args is not None:
+            message += f"\n- Command args: {shlex.join(command_args)}"
+
+        super().__init__(message)
+
+
+class _DockerExecution(Execution):
     """Docker execution."""
 
-    def __init__(self, metadata: Metadata, output_dir: pl.Path) -> None:
+    def __init__(
+        self, logger: logging.Logger, output_dir: pathlib.Path, metadata: Metadata
+    ) -> None:
         """Create DockerExecution."""
-        self.metadata = metadata
+        self.logger: logging.Logger = logger
         self.input_files: list[tuple[pl.Path, str]] = []
         self.input_file_next_id = 0
         self.output_files: list[tuple[pl.Path, str]] = []
         self.output_file_next_id = 0
         self.output_dir = output_dir
+        self.metadata = metadata
 
-    def input_file(self, host_file: pl.Path | str) -> str:
+    def input_file(self, host_file: InputPathType) -> str:
         """Resolve input file."""
         _host_file = pl.Path(host_file)
         local_file = f"/styx_input/{self.input_file_next_id}/{_host_file.name}"
@@ -41,7 +71,7 @@ class DockerExecution(Execution):
         self.input_files.append((_host_file, local_file))
         return local_file
 
-    def output_file(self, local_file: str, optional: bool = False) -> pl.Path:
+    def output_file(self, local_file: str, optional: bool = False) -> OutputPathType:
         """Resolve output file."""
         return self.output_dir / local_file
 
@@ -59,6 +89,13 @@ class DockerExecution(Execution):
 
         # Output directory
         self.output_dir.mkdir(parents=True, exist_ok=True)
+
+        # Create run script
+        run_script = self.output_dir / "run.sh"
+        # Ensure utf-8 encoding and unix newlines
+        run_script.write_text(
+            f"#!/bin/bash\n{shlex.join(cargs)}\n", encoding="utf-8", newline="\n"
+        )
 
         mounts.append("--mount")
         mounts.append(
@@ -84,27 +121,26 @@ class DockerExecution(Execution):
             "/bin/bash",
             *docker_extra_args,
             container,
-            "-l",
-            "-c",
-            " ".join(cargs),
+            "./run.sh",
         ]
 
-        print(f"Executing docker command: '{docker_command}'")
+        self.logger.debug(f"Running docker: {shlex.join(docker_command)}")
+        self.logger.debug(f"Running command: {shlex.join(cargs)}")
 
-        def stdout_handler(line: str) -> None:
-            print(line)
+        def _stdout_handler(line: str) -> None:
+            self.logger.info(line)
 
-        def stderr_handler(line: str) -> None:
-            print(line)
+        def _stderr_handler(line: str) -> None:
+            self.logger.error(line)
 
         with Popen(docker_command, text=True, stdout=PIPE, stderr=PIPE) as process:
             with ThreadPoolExecutor(2) as pool:  # two threads to handle the streams
                 exhaust = partial(pool.submit, partial(deque, maxlen=0))
-                exhaust(stdout_handler(line[:-1]) for line in process.stdout)  # type: ignore
-                exhaust(stderr_handler(line[:-1]) for line in process.stderr)  # type: ignore
+                exhaust(_stdout_handler(line[:-1]) for line in process.stdout)  # type: ignore
+                exhaust(_stderr_handler(line[:-1]) for line in process.stderr)  # type: ignore
         return_code = process.poll()
         if return_code:
-            raise CalledProcessError(return_code, process.args)
+            raise StyxDockerError(return_code, docker_command, cargs)
 
 
 def _default_execution_output_dir(metadata: Metadata) -> pl.Path:
@@ -116,24 +152,30 @@ def _default_execution_output_dir(metadata: Metadata) -> pl.Path:
 class DockerRunner(Runner):
     """Docker runner."""
 
-    def __init__(
-        self, execution_output_dir: Callable[[Metadata], pl.Path] | None = None
-    ) -> None:
-        """Create a new DockerRunner.
+    logger_name = "styx_docker_runner"
 
-        Args:
-            execution_output_dir: A function that returns the output
-                directory for a given Metadata.
-                If None, a folder named 'output_<metadata.name>' will be used.
-                This function is called once before every execution.
-        """
-        self.execution_output_dir: Callable[[Metadata], pl.Path] = (
-            _default_execution_output_dir
-            if execution_output_dir is None
-            else execution_output_dir
-        )
+    def __init__(self, data_dir: InputPathType | None = None) -> None:
+        """Create a new DockerRunner."""
+        self.data_dir = pathlib.Path(data_dir or "styx_tmp")
+        self.uid = os.urandom(8).hex()
+        self.execution_counter = 0
+
+        # Configure logger
+        self.logger = logging.getLogger(self.logger_name)
+        if not self.logger.hasHandlers():
+            self.logger.setLevel(logging.DEBUG)
+            ch = logging.StreamHandler()
+            ch.setLevel(logging.DEBUG)
+            formatter = logging.Formatter("[%(levelname).1s] %(message)s")
+            ch.setFormatter(formatter)
+            self.logger.addHandler(ch)
 
     def start_execution(self, metadata: Metadata) -> Execution:
         """Start execution."""
-        output_dir = self.execution_output_dir(metadata)
-        return DockerExecution(metadata, output_dir)
+        self.execution_counter += 1
+        return _DockerExecution(
+            logger=self.logger,
+            output_dir=self.data_dir
+            / f"{self.uid}_{self.execution_counter - 1}_{metadata.name}",
+            metadata=metadata,
+        )
